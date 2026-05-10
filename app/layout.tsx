42f21564605af3d726b0251bb559cb280f6dc792
Amiw <wms36155@gmail.com>
2026-05-10 15:57:30 +0500

    Shadcn UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,17 +17,19 @@ export const metadata: Metadata = {
   description: "A delicious pizza ordering app",
 };
 
+import ToastProvider from "@/providers/ToastProvider";
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body>
+        <ToastProvider />
+        {children}
+      </body>
     </html>
   );
 }
